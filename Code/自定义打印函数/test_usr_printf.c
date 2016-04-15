a36e24a21c3ef930a244ec1c3549c33873aa4e31
marvinkoala <982224886@qq.com>
2016-04-15 04:11:51 -0400

    【日志打印函数】新格式已改好

--- Code/自定义打印函数/test_usr_printf.c
+++ Code/自定义打印函数/test_usr_printf.c
@@ -4,8 +4,14 @@
 	> Mail: marvinkoala@google.com
 	> Created Time: Mon 11 Apr 2016 05:51:07 PM CST
  ************************************************************************/
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
 #include "type_def.h"
-#include "usr_printf.h"
+#include "log.h"
+
+char *make_msg(const char *fmt, ...);
+
 
 int main()
 {
@@ -13,14 +19,41 @@ int main()
     u8 *pcTestStr = "zao qi de chong er neng chi niao!";
     int32 i32TestVal = 23333;
 
+
+    //PrintBase("%s aaa bbb ccc %s\n", pcTestStr, paTest);
+
+    LogInfo("%p", paTest);
+    LogWarn("%s\n", pcTestStr);
+    LogError("%d\n", i32TestVal);
+    LogInfo("***************************");
+
     
-    UsrPrintf("%p\n", paTest);
-    UsrPrintf("%s\n", pcTestStr);
-    UsrPrintf("%d\n", i32TestVal);
-    UsrPrintf("***************************\n");
+    printf("\n");
 
 
-    UsrPrintf("%p %s %d\n", paTest, pcTestStr, i32TestVal);
+    LogInfo("%p %s %d\n", paTest, pcTestStr, i32TestVal);
+    LogWarn("%p %s %d\n", paTest, pcTestStr, i32TestVal);
+    LogError("%p %s %d\n", paTest, pcTestStr, i32TestVal);
+    
 
     return 0;
+}
+
+
+char *make_msg(const char *fmt, ...)
+{
+    int size,n;
+    char p[1024]={0};
+    va_list ap;
+
+
+    //p = (char *)malloc(size);
+    
+
+    va_start(ap, fmt);
+    n=vsnprintf(p, 1024, fmt, ap);
+    va_end(ap);
+
+
+    return NULL;
 }
