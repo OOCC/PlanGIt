ca740495438c5b574b8332675ada5599ff4f80c8
yuxue <xy741@126.com>
2017-12-21 23:11:24 +0800

    机器学习，第一周，梯度下降，标准方程

--- Code/算法练习/进阶算法/atoi and itoa/atoiAndItoa.cpp
+++ Code/算法练习/进阶算法/atoi and itoa/atoiAndItoa.cpp
@@ -7,30 +7,30 @@ long __cdecl atol(  
 const char *nptr  
 )  
 {  
-int c;  
-long total;  
-int sign;  
-  
-  
-while ( isspace((int)(unsigned char)*nptr) )  
-++nptr;  
-  
-c = (int)(unsigned char)*nptr++;  
-sign = c;  
-if (c == ''-'' || c == ''+'')  
-c = (int)(unsigned char)*nptr++;  
-  
-total = 0;  
-  
-while (isdigit(c)) {  
-total = 10 * total + (c - ''0'');  
-c = (int)(unsigned char)*nptr++;  
-}  
-  
-if (sign == ''-'')  
-return -total;  
-else  
-return total;  
+    int c;  
+    long total;  
+    int sign;  
+      
+      
+    while ( isspace((int)(unsigned char)*nptr) )  
+        ++nptr;  
+      
+    c = (int)(unsigned char)*nptr++;  
+    sign = c;  
+    if (c == ''-'' || c == ''+'')  
+        c = (int)(unsigned char)*nptr++;  
+      
+    total = 0;  
+      
+    while (isdigit(c)) {  
+        total = 10 * total + (c - ''0'');  
+        c = (int)(unsigned char)*nptr++;  
+    }  
+      
+    if (sign == ''-'')  
+        return -total;  
+    else  
+        return total;  
 }  
   
   
@@ -83,42 +83,42 @@ return total;  
 #include <msvcrt/internal/file.h>  
 char* _itoa(int value, char* string, int radix)  
 {  
-char tmp[33];  
-char* tp = tmp;  
-int i;  
-unsigned v;  
-int sign;  
-char* sp;  
+    char tmp[33];  
+    char* tp = tmp;  
+    int i;  
+    unsigned v;  
+    int sign;  
+    char* sp;  
   
-if (radix > 36 || radix <= 1)  
-{  
-__set_errno(EDOM);  
-return 0;  
-}  
+    if (radix > 36 || radix <= 1)  
+    {  
+        __set_errno(EDOM);  
+        return 0;  
+    }  
   
-sign = (radix == 10 && value < 0);  
-if (sign)  
-v = -value;  
-else  
-v = (unsigned)value;  
-while (v || tp == tmp)  
-{  
-i = v % radix;  
-v = v / radix;  
-if (i < 10)  
-*tp++ = i+''0'';  
-else  
-*tp++ = i + ''a'' - 10;  
-}  
+    sign = (radix == 10 && value < 0);  
+    if (sign)  
+        v = -value;  
+    else  
+        v = (unsigned)value;  
+    while (v || tp == tmp)  
+    {  
+        i = v % radix;  
+        v = v / radix;  
+        if (i < 10)  
+            *tp++ = i+''0'';  
+        else  
+            *tp++ = i + ''a'' - 10;  
+    }  
   
-if (string == 0)  
-string = (char*)malloc((tp-tmp)+sign+1);  
-sp = string;  
-  
-if (sign)  
-*sp++ = ''-'';  
-while (tp > tmp)  
-*sp++ = *--tp;  
-*sp = 0;  
-return string;  
+    if (string == 0)  
+    string = (char*)malloc((tp-tmp)+sign+1);  
+    sp = string;  
+      
+    if (sign)  
+    *sp++ = ''-'';  
+    while (tp > tmp)  
+        *sp++ = *--tp;  
+    *sp = 0;  
+    return string;  
 } </span>  
