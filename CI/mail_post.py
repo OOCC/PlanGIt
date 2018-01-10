b815517a15380b487f84eaccb5730aaf896410c0
marvinkoala <marvinkoala@126.com>
2018-01-10 10:02:32 +0800

    测试yuxue的发邮件钩子

--- CI/mail_post.py
+++ CI/mail_post.py
@@ -38,7 +38,7 @@ def disk_space():
 def send_mail(sub, content):
     ############# 
     # 要发给谁，这里发给1个人
-    mailto_list = ["xueuy@qq.com, 635541030@qq.com, 982224886@qq.com, yyf050547@qq.com, dongzhixiangboy@qq.com"]
+    mailto_list = ["xueuy@qq.com, 635541030@qq.com, 982224886@qq.com, yyf050547@qq.com, dongzhixiangboy@qq.com, 1160798981@qq.com, marvin_xie@126.com"]
     ##################### 
     # 设置服务器，用户名、口令以及邮箱的后缀
     mail_host = "smtp.qq.com"
